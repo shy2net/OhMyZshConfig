b2520184c5ae118a86b274e38054be12447b5a88
shynet <shynet@gmail.com>
2019-05-25 00:47:11 +0300

    First init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Introduction
+
+This is my installation and configurations of oh-my-zsh for Mac. Calling the install.sh script will
+install oh-my-zsh with iTerm2.
+
+It includes the following:
+
+- iTerm 2
+- oh-my-zsh
+- Meslo font
+- Powerlevel9k
+
+Simply call:
+> ./install.sh
---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install iterm2
+brew cask install iterm2
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Clone popwerlevel9k to allow showing exit codes and timestamps
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Copy all of the fonts (system wide usage)
+cp -Rf ./fonts/* /Library/Fonts
